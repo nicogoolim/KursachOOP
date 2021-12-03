dc375a24f3d54d2b75ac4bbc60890ac63184557a
Михаил Скорых <18001836@edu.spbgasu.ru>
2021-12-03 20:16:14 +0300

    Добавьте файлы проекта.

---
+++ MFCApplication2/MFCApplication2.h
@@ -0,0 +1,32 @@
+﻿
+// MFCApplication2.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CMFCApplication2App:
+// Сведения о реализации этого класса: MFCApplication2.cpp
+//
+
+class CMFCApplication2App : public CWinApp
+{
+public:
+	CMFCApplication2App();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCApplication2App theApp;
---
+++ MFCApplication2/MFCApplication2Dlg.h
@@ -0,0 +1,34 @@
+﻿
+// MFCApplication2Dlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CMFCApplication2Dlg
+class CMFCApplication2Dlg : public CDialogEx
+{
+// Создание
+public:
+	CMFCApplication2Dlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
